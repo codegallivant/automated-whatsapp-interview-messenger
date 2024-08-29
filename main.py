4d6cf2dc59694f4248d863332cc0e33ece82ad1a
codegallivant <janak3.1415et@gmail.com>
2024-08-30 03:38:51 +0530

    update

--- main.py
+++ main.py
@@ -1,39 +1,104 @@
-# import pywhatkit
 import pandas as pd
 import yaml
 import traceback
 import datetime
 import time
 from alright import WhatsApp
+import sys
+
+
+def permission_to_continue():
+    while True:
+        print()
+        response = input("Continue execution? (y/n) ")
+        print()
+        if response in ["y","Y"]:
+            return
+        elif response in ["n","N"]:
+            sys.exit()    
+
+
+def print_params(params):
+    print()
+    print("Parameters:")
+    for param in params.keys():
+        if isinstance(params[param], dict):
+            print()
+            print(param)
+            print_params(params[param])
+        else:
+            print(param, ":", params[param])
+
 
 with open("parameters.yaml") as stream:
     try:
         PARAMS = yaml.safe_load(stream)
-        print(PARAMS)
+        print_params(PARAMS)
+        print()
     except yaml.YAMLError as exc:
         print(exc)
 
+permission_to_continue()
 
 details = pd.read_csv("details.csv")
 
+print("First 5 rows of 'details.csv':")
+print(details.head())
+permission_to_continue()
+
+
+def parse_template_message():
+    index_pairs = list()
+    start_index = None
+    end_index = None
+    open = True
+    for i, character in enumerate(template_message):
+        if character == "$":
+            if i < len(template_message)-1:
+                if template_message[i+1]=='{':
+                    open = True
+                    start_index = i 
+        if open == True and character == "}":
+            open = False
+            end_index = i
+            index_pairs.append((start_index, end_index))
+    return index_pairs
+
+with open("message.txt") as f:
+    template_message = f.read()
+    print("Contents of 'message.txt':")
+    print(template_message)
+index_pairs = parse_template_message()
+
 
 def synthesise_message(name, subsystem, date, interview_time):
-    message_parts = [
-"Hello ",
-name,
-". Greetings from Project MANAS. Your interview for ",
-subsystem,
-" is scheduled for ",
-date,
-" at ",
-interview_time,
-"."
-]
-    return "".join(message_parts)
+    variables = {
+        "name": name,
+        "subsystem": subsystem,
+        "date": date,
+        "interview_time": interview_time
+    }
+    # print(index_pairs)
+    message = ""
+    message += template_message[:index_pairs[0][0]]
+    message += variables[template_message[index_pairs[0][0]+2:index_pairs[0][1]]]
+    # print(message)
+    for i in range(1, len(index_pairs)):
+        index_pair = index_pairs[i]
+        message += template_message[index_pairs[i-1][1]+1:index_pair[0]]
+        # print(message)
+        message += variables[template_message[index_pair[0]+2:index_pair[1]]]
+        # print(message)
+    message += template_message[index_pairs[-1][1]+1:]
+    # print(message)
+    return message
 
+print("Example message:")
+print(synthesise_message("Abc Dfg", "Artificial Intelligence", "27/08/2024", "19:00"))
+
+permission_to_continue()
 
 messenger = WhatsApp()
-
 def send_message(name, phone_number, message):
     phone_number = str(phone_number)
     if len(phone_number)==10:
@@ -52,61 +117,67 @@ def send_message(name, phone_number, message):
             print(traceback.format_exc())
 
 
-
 def calculate_time(i, start_time, end_time, duration, padding_minutes):
     hours = int(start_time.split(":")[0])
     minutes = int(start_time.split(":")[1])
+    
     minutes_increase = i*(duration+padding_minutes)
+    
     this_start_datetime_obj = (datetime.datetime.combine(datetime.date.today(), datetime.time(hours, minutes))+datetime.timedelta(minutes=minutes_increase))
     this_start_time_obj = this_start_datetime_obj.time()
-    new_hours = this_start_time_obj.hour
-    new_minutes = this_start_time_obj.minute
+    this_start_time_hours = this_start_time_obj.hour
+    this_start_time_minutes = this_start_time_obj.minute
 
-    print(hours, minutes)
     this_end_datetime_obj = this_start_datetime_obj + datetime.timedelta(minutes=duration)
     this_end_time_obj = this_end_datetime_obj.time()
-    this_end_hour = this_end_time_obj.hour
-    this_end_minutes = this_end_time_obj.minute
-    # minutes_increase = minutes + i*(duration+padding_minutes)
-    # hours_increase = minutes//60
-    # new_minutes = minutes_increase % 60
-    # new_hours = hours+hours_increase
-    # new_time = t + datetime.timedelta(minutes = minutes_increase)
-    # new_hours = new_time.hours
-    # new_minutes = new_time.minutes
+    this_end_time_hour = this_end_time_obj.hour
+    this_end_time_minutes = this_end_time_obj.minute
     end_hour = int(end_time.split(":")[0])
     end_minutes = int(end_time.split(":")[1])
-    if this_end_hour == end_hour:
-        if this_end_minutes >= end_minutes:
-            return -1
-    if this_end_hour > end_hour:
-        return -1
-    if new_minutes < 10:
-        new_minutes = "0"+str(new_minutes)
-    interview_time = str(new_hours)+":"+str(new_minutes)
-    print(interview_time)
+    
+    if this_end_time_hour == end_hour:
+        if this_end_time_minutes >= end_minutes:
+            return False
+    if this_end_time_hour > end_hour:
+        return False
+    
+    if this_start_time_minutes < 10:
+        this_start_time_minutes = "0"+str(this_start_time_minutes)
+    
+    interview_time = str(this_start_time_hours)+":"+str(this_start_time_minutes)
+    # print(interview_time)
+    
     return interview_time
 
 
-# Send all messages
+def validate_row(first_year, subsystem):
+    return (first_year == "Yes") and (subsystem == PARAMS["target_subsystem"] or PARAMS["target_subsystem"] in [None, ""])
 
+
+# Send all messages
 i = 0
+batch_count = 0
 for index, row in details.iterrows():
-    print(row)
-    name = row["Full Name"]
-    phone_number = row["WhatsApp Number"]
-    registration_numbers = row["Registration No. "]
-    preference_columns = ["First Preference of Subsystem", "Second Preference of Subsystem"]
+    # print(row)
+    name = row[PARAMS['columns']['name']]
+    phone_number = row[PARAMS['columns']['whatsapp_number']]
+    preference_columns = [PARAMS["columns"]["preference1"], PARAMS["columns"]["preference2"]]
     subsystem = row[preference_columns[PARAMS["subsystem_preference"]-1]]
-    first_year = row["This form is only for First Year Students. Are you in First Year?"]
-    if first_year != "Yes":
+    first_year = row[PARAMS["columns"]["first_year"]]
+
+    if not validate_row(first_year, subsystem):
+        print("Skipping..")
         continue
-    interview_time = calculate_time(i, PARAMS["start_time"], PARAMS["end_time"], PARAMS["duration"], PARAMS["padding_minutes"])
-    if interview_time == -1:
+    
+    if i%PARAMS["at_once"] == 0:
+       interview_time = calculate_time(batch_count, PARAMS["start_time"], PARAMS["end_time"], PARAMS["duration"], PARAMS["padding_minutes"])
+       batch_count += 1
+    if interview_time == False:
         print("End time reached.")
         print(f"{i} interviews scheduled.")
         break
     message = synthesise_message(name, subsystem, PARAMS["date"], interview_time)
     send_message(name, phone_number, message)
-    time.sleep(PARAMS["message_interval"])
+    print(f"{index}: Interview", "for", subsystem,"scheduled at", interview_time+".", f"Message sent to {name}(Whatsapp number: {str(phone_number)}).")
     i+=1
+    time.sleep(PARAMS["message_interval"])
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ Automatic whatsapp messenger & scheduler for interviews at project MANAS
 
 ## Dependencies
 1. ``pip install -r requirements.txt``
-2. Install a stable chrome and chrome driver version from ``https://googlechromelabs.github.io/chrome-for-testing/``. (I used version 128). This is required as automated scripts can only work with chrome browsers in testing.
+2. Install a stable chrome and chrome driver version from [Chrome for Testing](https://googlechromelabs.github.io/chrome-for-testing/). (I used version 128). This is required as automated scripts can only work with chrome browsers in testing.
 3. Modify the python ``alright`` package as follows - 
     \
     In ``site-packages/alright/__init__.py``, replace the indented block under ``if not browser:`` with:
@@ -19,6 +19,14 @@ Automatic whatsapp messenger & scheduler for interviews at project MANAS
             browser.close()
     ```
 
+## Set template message
+Modify/create a file called "message.txt" in the same directory as the script and set the contents. Use ${variable_name} for variables like ``name`` or ``interview_time`` etc. For example:
+```
+Hello ${name}. Greetings from Project MANAS. Your interview for ${subsystem} is scheduled for ${date} at ${interview_time}.
+```
+
 ## Run
-1. Add interviewee details in details.csv file
-2. ``python3 main.py``
+1. Add interviewee details in a file called ``details.csv`` in the same directory
+2. ```bash
+python3 main.py
+```
